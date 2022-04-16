fa57e00c085a5554f11082d151d29459b8035299
Zeron <aaisalu@users.noreply.github.com>
2022-04-16 11:06:53 +0545

    added useful usage

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,4 +1,9 @@
 import signal
+import psutil
+import platform
+from telegram import Update, Bot, Message, Chat, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup, version as pybot
+
+
 
 from os import path as ospath, remove as osremove, execl as osexecl
 from subprocess import run as srun
@@ -10,6 +15,10 @@ from sys import executable
 from telegram import ParseMode, InlineKeyboardMarkup
 from telegram.ext import CommandHandler
 
+from datetime import datetime
+from platform import python_version, uname
+
+
 from wserver import start_server_async
 from bot import bot, app, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, IS_VPS, PORT, alive, web, OWNER_ID, AUTHORIZED_CHATS, LOGGER, Interval, nox, rss_session, a2c
 from .helper.ext_utils.fs_utils import start_cleanup, clean_all, exit_clean_up
@@ -19,14 +28,7 @@ from .helper.ext_utils.telegraph_helper import telegraph
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
 from .helper.telegram_helper.button_build import ButtonMaker
-from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, speedtest, count, leech_settings, search, rss
-
-
-# Testing
-import psutil
-import platform
-from datetime import datetime
-from platform import python_version, uname
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, speedtest, count, leech_settings, search, rss ,ud,covid,cash,weather,password,gtts
 
 
 def stats(update, context):
@@ -45,43 +47,56 @@ def stats(update, context):
     swap_t = get_readable_file_size(swap.total)
     swap_u = get_readable_file_size(swap.used)
     memory = virtual_memory()
+    uname = platform.uname()
     mem_p = memory.percent
+    boot_time_timestamp = psutil.boot_time()
+    bt = datetime.fromtimestamp(boot_time_timestamp)
     mem_t = get_readable_file_size(memory.total)
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
-    stats = f'<b>Bot Uptime:</b> {currentTime}\n\n'\
-            f'<b>Total Disk Space:</b> {total}\n'\
-            f'<b>Used:</b> {used} | <b>Free:</b> {free}\n\n'\
-            f'<b>Upload:</b> {sent}\n'\
-            f'<b>Download:</b> {recv}\n\n'\
-            f'<b>CPU:</b> {cpuUsage}%\n'\
+    stats = f"<b>Zeron information 🛠</b>\n"\
+            f'<b>Zeron Uptime:</b> {currentTime}\n'\
+            f"<b>Python:</b>  {python_version()} \n"\
+            f"<b>Python Tg ver:</b>  {pybot.__version__} \n\n"\
+            f'<b>CPU status:</b> {cpuUsage}%\n'\
             f'<b>RAM:</b> {mem_p}%\n'\
             f'<b>DISK:</b> {disk}%\n\n'\
+            f"<b>System information 🦚</b>\n"\
+            f'<b>Base OS:</b> {uname.system} \n'\
+            f"<b>Version:</b> {uname.version} \n"\
+            f"<b>Release:</b> {uname.release} \n"\
+            f"<b>Processor:</b> {uname.processor} \n"\
+            f"<b>Boot time:</b> {bt.day}/{bt.month}/{bt.year} - {bt.hour}:{bt.minute}:{bt.second} \n"\
+            f"<b>CPU cores:</b> {psutil.cpu_count(logical=False)} physical, {psutil.cpu_count()} logical \n"\
+            f"<b>CPU freq:</b> {psutil.cpu_freq().current:.2f}Mhz \n"\
+            f"<b>CPU usage:</b> {psutil.cpu_percent()}% \n"\
             f'<b>Physical Cores:</b> {p_core}\n'\
-            f'<b>Total Cores:</b> {t_core}\n\n'\
-            f'<b>SWAP:</b> {swap_t} | <b>Used:</b> {swap_p}%\n'\
+            f'<b>Total Cores:</b> {t_core}\n'\
             f'<b>Memory Total:</b> {mem_t}\n'\
-            f'<b>Memory Free:</b> {mem_a}\n'\
-            f'<b>Memory Used:</b> {mem_u}\n'
+            f'<b>Brian Free:</b> {mem_a}\n'\
+            f'<b>Brian Used:</b> {mem_u}\n'\
+            f'<b>SWAP:</b> {swap_t} || <b>Used:</b> {swap_p}%\n\n'\
+            f'<b>Upload:</b> {sent} || <b>Download:</b> {recv}\n'\
+            f'<b>Total Disk Space:</b> {total}\n'\
+            f'<b>Used:</b> {used} || <b>Free:</b> {free}\n'
     sendMessage(stats, context.bot, update)
 
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+MwgSi5vmQEA2N2Vk")
+    buttons.buildbutton("One-Love", "https://bit.ly/3ud1k8l")
+    buttons.buildbutton("One-Heart", "https://t.me/zeroncell")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive!
-Type /{BotCommands.HelpCommand} to get a list of available commands
+Junkiri doesn't want you to love by proxy.
 '''
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
-        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update, reply_markup)
+        sendMarkup('No Flirting Please: Join @zeroncell ) ', context.bot, update, reply_markup)
 
 def restart(update, context):
-    restart_message = sendMessage("Restarting...", context.bot, update)
+    restart_message = sendMessage("Junkiri Restarting...", context.bot, update)
     if Interval:
         Interval[0].cancel()
     alive.kill()
@@ -178,20 +193,20 @@ help_string_telegraph = f'''<br>
 <br>sites: <code>rarbg, 1337x, yts, etzv, tgx, torlock, piratebay, nyaasi, ettv</code><br><br>
 <b>/{BotCommands.StatusCommand}</b>: Shows a status of all the downloads
 <br><br>
-<b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
+<b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the Junkiri is hosted on
 '''
 
 help = telegraph.create_page(
-        title='Mirror-Leech-Bot Help',
+        title='Junkiri Help',
         content=help_string_telegraph,
     )["path"]
 
 help_string = f'''
-/{BotCommands.PingCommand}: Check how long it takes to Ping the Bot
+/{BotCommands.PingCommand}: Check how long it takes to Ping the Junkiri
 
-/{BotCommands.AuthorizeCommand}: Authorize a chat or a user to use the bot (Can only be invoked by Owner & Sudo of the bot)
+/{BotCommands.AuthorizeCommand}: Authorize a chat or a user to use the Junkiri (Can only be invoked by Owner & Sudo of the Junkiri)
 
-/{BotCommands.UnAuthorizeCommand}: Unauthorize a chat or a user to use the bot (Can only be invoked by Owner & Sudo of the bot)
+/{BotCommands.UnAuthorizeCommand}: Unauthorize a chat or a user to use the Junkiri (Can only be invoked by Owner & Sudo of the Junkiri)
 
 /{BotCommands.AuthorizedUsersCommand}: Show authorized users (Only Owner & Sudo)
 
@@ -199,9 +214,9 @@ help_string = f'''
 
 /{BotCommands.RmSudoCommand}: Remove sudo users (Only Owner)
 
-/{BotCommands.RestartCommand}: Restart and update the bot
+/{BotCommands.RestartCommand}: Restart and update the Junkiri
 
-/{BotCommands.LogCommand}: Get a log file of the bot. Handy for getting crash reports
+/{BotCommands.LogCommand}: Get a log file of the Junkiri. Handy for getting crash reports
 
 /{BotCommands.SpeedCommand}: Check Internet Speed of the Host
 
@@ -212,7 +227,7 @@ help_string = f'''
 
 def bot_help(update, context):
     button = ButtonMaker()
-    button.buildbutton("Other Commands", f"https://telegra.ph/{help}")
+    button.buildbutton("Junkiri Commands", f"https://telegra.ph/{help}")
     reply_markup = InlineKeyboardMarkup(button.build_menu(1))
     sendMarkup(help_string, context.bot, update, reply_markup)
 
@@ -259,15 +274,12 @@ def main():
     if ospath.isfile(".restartmsg"):
         with open(".restartmsg") as f:
             chat_id, msg_id = map(int, f)
-        bot.edit_message_text("Restarted successfully!", chat_id, msg_id)
+        bot.edit_message_text("Junkiri Restarted successfully!", chat_id, msg_id)
         osremove(".restartmsg")
     elif OWNER_ID:
         try:
-            text = "<b>Bot Restarted!</b>"
+            text = "<b>Junkiri Restarted!</b>"
             bot.sendMessage(chat_id=OWNER_ID, text=text, parse_mode=ParseMode.HTML)
-            if AUTHORIZED_CHATS:
-                for i in AUTHORIZED_CHATS:
-                    bot.sendMessage(chat_id=i, text=text, parse_mode=ParseMode.HTML)
         except Exception as e:
             LOGGER.warning(e)
 
@@ -288,7 +300,7 @@ def main():
     dispatcher.add_handler(stats_handler)
     dispatcher.add_handler(log_handler)
     updater.start_polling(drop_pending_updates=IGNORE_PENDING_REQUESTS)
-    LOGGER.info("Bot Started!")
+    LOGGER.info("Junkiri Started!")
     signal.signal(signal.SIGINT, exit_clean_up)
     if rss_session is not None:
         rss_session.start()
