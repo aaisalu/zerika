0c69471cb5600951fdf33cd4f6f8b1805fbc9aaa
Zeron <aaisalu@users.noreply.github.com>
2022-04-30 20:11:55 +0545

    Advanced speedtest kanged lol

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -1,34 +1,27 @@
-from speedtest import Speedtest
-from telegram.ext import CommandHandler
-
+import speedtest
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot import dispatcher
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot.helper.telegram_helper.message_utils import sendMessage, editMessage
+from bot.helper.telegram_helper.message_utils import sendMessage, editMessage,deleteMessage,sendMarkup
+from telegram.ext import CommandHandler, CallbackQueryHandler
 
 
-def speedtest(update, context):
-    speed = sendMessage("Running Speed Test . . . ", context.bot, update)
-    test = Speedtest()
-    test.get_best_server()
-    test.download()
-    test.upload()
-    test.results.share()
-    result = test.results.dict()
-    string_speed = f'''
-<b>Server</b>
-<b>Name:</b> <code>{result['server']['name']}</code>
-<b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
-<b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
-<b>ISP:</b> <code>{result['client']['isp']}</code>
 
-<b>SpeedTest Results</b>
-<b>Upload:</b> <code>{speed_convert(result['upload'] / 8)}</code>
-<b>Download:</b>  <code>{speed_convert(result['download'] / 8)}</code>
-<b>Ping:</b> <code>{result['ping']} ms</code>
-<b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
-'''
-    editMessage(string_speed, speed)
+def convert(speed):
+    return round(int(speed) / 1048576, 2)
+
+
+def speedtestxyz(update, context):
+    buttons = [
+        [
+            InlineKeyboardButton("Cool Image", callback_data="speedtest_image"),
+            InlineKeyboardButton("Preety Text", callback_data="speedtest_text"),
+        ]
+    ]
+    update.effective_message.reply_text(
+        "🛠 Select SpeedTest Mode", reply_markup=InlineKeyboardMarkup(buttons)
+    )
 
 
 def speed_convert(size):
@@ -42,7 +35,46 @@ def speed_convert(size):
     return f"{round(size, 2)} {units[zero]}"
 
 
-SPEED_HANDLER = CommandHandler(BotCommands.SpeedCommand, speedtest,
+
+def speedtestxyz_callback(update, context):
+    query = update.callback_query
+    msg = update.effective_message.edit_text("📡 Running a speedtest....")
+    speed = speedtest.Speedtest()
+    speed.get_best_server()
+    speed.download()
+    speed.upload()
+    replymsg = "⚡️ SpeedTest Results:"
+    if query.data == "speedtest_image":
+        speedtest_image = speed.results.share()
+        update.effective_message.reply_photo(
+            photo=speedtest_image, caption=replymsg
+        )
+        msg.delete()
+    elif query.data == "speedtest_text":
+        result = speed.results.dict()
+        string_speed = f'''
+<b>🛠 Zeron Server</b>
+<b>Name:</b> <code>{result['server']['name']}</code>
+<b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+<b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
+<b>ISP:</b> <code>{result['client']['isp']}</code>
+
+<b>SpeedTest Results</b>
+<b>Upload:</b> <code>{speed_convert(result['upload'] / 8)}</code>
+<b>Download:</b>  <code>{speed_convert(result['download'] / 8)}</code>
+<b>Ping:</b> <code>{result['ping']} ms</code>
+<b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+'''
+        # string_speed += f"\nDownload: `{convert(result['download'])}Mb/s`\nUpload: `{convert(result['upload'])}Mb/s`\nPing: `{result['ping']}`"
+        # update.effective_message.edit_text(string_speed)
+
+        editMessage(string_speed, msg)
+
+
+
+SPEED_HANDLER = CommandHandler(BotCommands.SpeedCommand, speedtestxyz,
                                                   filters=CustomFilters.owner_filter | CustomFilters.authorized_user, run_async=True)
+Speed_result_handler = CallbackQueryHandler(speedtestxyz_callback, pattern="speedtest_.*", run_async=True)
 
 dispatcher.add_handler(SPEED_HANDLER)
+dispatcher.add_handler(Speed_result_handler)
